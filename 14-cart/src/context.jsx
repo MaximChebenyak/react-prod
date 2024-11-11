d6be7980a81b98afddf8278ac9650d3c4e239ba6
maxim <maximchebenyak@gmail.com>
2024-11-11 18:37:32 +0200

    fetch data

--- 14-cart/src/context.jsx
+++ 14-cart/src/context.jsx
@@ -11,15 +11,13 @@ import {
 import cartItems from './data';
 import { getTotals } from './utils';
 
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
+
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: new Map(
-    cartItems.map((item) => {
-      return [item.id, item];
-    })
-  ),
+  cart: new Map(),
 };
 
 export const AppProvider = ({ children }) => {
@@ -41,6 +39,17 @@ export const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } });
   };
+
+  const fetchData = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <AppContext.Provider
