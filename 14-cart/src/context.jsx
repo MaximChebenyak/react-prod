66f6460c7818c5233d8e5604a12e9201d66b34e6
maxim <maximchebenyak@gmail.com>
2024-11-11 17:24:57 +0200

    calculate totals

--- 14-cart/src/context.jsx
+++ 14-cart/src/context.jsx
@@ -8,19 +8,54 @@ import {
   LOADING,
   DISPLAY_ITEMS,
 } from './actions';
+import cartItems from './data';
+import { getTotals } from './utils';
 
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: [],
+  cart: new Map(
+    cartItems.map((item) => {
+      return [item.id, item];
+    })
+  ),
 };
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { totalAmount, totalCost } = getTotals(state.cart);
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  const remove = (id) => {
+    dispatch({ type: REMOVE, payload: { id } });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: INCREASE, payload: { id } });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: DECREASE, payload: { id } });
+  };
 
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 };
 
